dd2ee08d6e976ac84231d901a3c24d5cdebf5658
YakshitaRathod <72180197+YakshitaRathod@users.noreply.github.com>
2020-10-03 03:21:56 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,7 +1,6 @@
-def kSmallest(arr, k):
-arr.sort(reverse=False)
-for i in range(k):
-    print (arr[i],end=" ") 
-arr = [9,7,6,8,5,3,2,4,1]
-k = int(input(""))
-kSmallest(arr, k)
+arr = [9,7,6,8,5,3,2,4,1,0]
+def kSmallest(arr):
+    arr.sort(reverse=False)
+    for i in arr:
+        print (arr[i])
+kSmallest(arr)
